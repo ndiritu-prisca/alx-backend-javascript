fa7dddbae957b49daf61ac5efdaf5ae9634c2738
ndiritu-prisca <priscandiritu@outlook.com>
2023-07-25 04:44:42 +0300

    Use const

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    /* eslint-disable no-unused-vars */
+    const task = true;
+    const task2 = false;
+    /* eslint-disable no-unused-vars */
   }
 
   return [task, task2];
 }
-
