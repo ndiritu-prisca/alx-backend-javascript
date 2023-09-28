57c9e488da595bc9f05a4bbd7a7a6759638b6a94
ndiritu-prisca <priscandiritu@outlook.com>
2023-09-29 00:44:11 +0300

    Return a new object

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,39 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(newBrand) {
+    this._brand = newBrand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(newMotor) {
+    this._motor = newMotor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(newColor) {
+    this._color = newColor;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    return new this.constructor[Symbol.species]();
+  }
+}
