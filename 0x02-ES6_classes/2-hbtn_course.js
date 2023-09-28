b2df4f4e902aaef19660a6a09013f44625f02eae
ndiritu-prisca <priscandiritu@outlook.com>
2023-09-28 16:00:57 +0300

    Use getter and setter

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,54 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    } else if (typeof length !== 'number') {
+      throw TypeError('Length must be a number');
+    } else if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
+      throw TypeError('Students must be an array of strings');
+    } else {
+      this._name = name;
+      this._length = length;
+      this._students = students;
+    }
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Setter for name
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  // Getter for length
+  get length() {
+    return this._length;
+  }
+
+  // Setter for length
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    this._length = newLength;
+  }
+
+  // Getter for students
+  get students() {
+    return this._students;
+  }
+
+  // Setter for students
+  set students(newStudents) {
+    if (!Array.isArray(newStudents) || !newStudents.every((student) => typeof student === 'string')) {
+      throw TypeError('Students must be an array of strings');
+    }
+    this._students = newStudents;
+  }
+}
