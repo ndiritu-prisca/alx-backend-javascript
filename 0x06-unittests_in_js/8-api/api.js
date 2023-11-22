4f4e4720a6805685064e3e6e7dd81e04fe475e5b
ndiritu-prisca <priscandiritu@outlook.com>
2023-11-23 01:28:28 +0300

    Change template to string

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`API available on localhost port ${port}`);
+  console.log('API available on localhost port 7865');
 });
 
 module.exports = app;
