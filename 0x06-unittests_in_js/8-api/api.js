c9b52b2d75cbadf958e68575746ea3456ef58bd3
ndiritu-prisca <priscandiritu@outlook.com>
2023-11-23 01:26:22 +0300

    Basic integration testing

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 7865;
 
 app.get('/', (req, res) => {
-  res.status(200).send('Welcome to the payment system');
+  res.send('Welcome to the payment system');
 });
 
 app.listen(port, () => {
