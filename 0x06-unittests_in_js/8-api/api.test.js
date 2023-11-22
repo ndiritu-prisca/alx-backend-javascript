6c4ad84fa029c0e3144374f814fac7560ed153b4
ndiritu-prisca <priscandiritu@outlook.com>
2023-11-23 01:34:55 +0300

    Remove if statement

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -2,12 +2,11 @@ const { expect } = require('chai');
 const request = require('request');
 
 describe('Index page', () => {
-  it('test the API with status, body', (done) => {
+  it('responds with status 200 and correct result for GET /', (done) => {
     request('http://localhost:7865', 'GET', (error, res, msg) => {
-      if (error) throw error;
       expect(res.statusCode).to.equal(200);
       expect(msg).to.equal('Welcome to the payment system');
+      done();
     });
-    done();
   });
 });
